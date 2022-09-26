03616a2a28a39716e3da1681af182f7fc8dc5bca
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-09-26 23:02:28 +0300

    solved task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+playFromCurrentTime();
+
+function playFromCurrentTime() {
+  const saveData = JSON.parse(localStorage.getItem(CURRENT_TIME_KEY));
+
+  if (!saveData) {
+    return;
+  }
+
+  player.setCurrentTime(saveData);
+}
+
+function onTimeUpdate(data) {
+  localStorage.setItem(CURRENT_TIME_KEY, data.seconds);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
+import { save, load, remove } from './storage';
 
+const CURRENT_FORM_VALUE = 'feedback-form-state';
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+initPage();
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmint);
+
+function onFormInput(evt) {
+  const { name, value } = evt.target;
+
+  let saveData = load(CURRENT_FORM_VALUE);
+  saveData = saveData ? saveData : {};
+
+  saveData[name] = value;
+  save(CURRENT_FORM_VALUE, saveData);
+}
+function initPage() {
+  const saveData = load(CURRENT_FORM_VALUE);
+
+  if (!saveData) {
+    return;
+  }
+
+  Object.entries(saveData).forEach(([name, value]) => {
+    refs.form.elements[name].value = value;
+  });
+}
+
+function onFormSubmint(evt) {
+  evt.preventDefault();
+
+  const { email, message } = evt.currentTarget.elements;
+
+  console.log({ email: email.value, message: message.value });
+  evt.currentTarget.reset();
+  remove(CURRENT_FORM_VALUE);
+}
